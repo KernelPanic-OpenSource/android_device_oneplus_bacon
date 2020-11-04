a2dea167da9b9b8b34901deaf2849a92f19a4c83
Florian Leeber <flori@bin.org.in>
2020-11-04 20:34:57 +0100

    Disable cache partition (too small), replace with quirks
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -26,3 +26,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.bacon.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/recovery.bacon.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
