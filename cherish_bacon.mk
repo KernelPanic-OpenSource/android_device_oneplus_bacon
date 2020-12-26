53784a4b246a0389c430a0a62b922cb296893a6f
Nobi Nobita <43093213+dopaemon@users.noreply.github.com>
2020-12-26 12:12:47 +0700

    Changle: Common Full Phone For Bacon

--- cherish_bacon.mk
+++ cherish_bacon.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cherish/config/common_mini_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 PRODUCT_NAME := cherish_bacon
 PRODUCT_DEVICE := bacon
