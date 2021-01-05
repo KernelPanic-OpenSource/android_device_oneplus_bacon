10de4a2a15df5d4add8d8058896b636a0f7bfded
dopaemon <polarisdp@gmail.com>
2021-01-05 14:47:03 +0700

    Multirom: Update
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- common/oppo/common/BoardConfigCommon.mk
+++ common/oppo/common/BoardConfigCommon.mk
@@ -26,4 +26,4 @@ TARGET_POWERHAL_VARIANT := qcom
 
 # Releasetools
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_oppo
-TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/common
+TARGET_RELEASETOOLS_EXTENSIONS := device/oneplus/bacon/common/oppo/common
